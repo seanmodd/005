71d6f5451048d50cdd82cc02ad756b7270eb6d61
Sean <sean@vestaridefair.com>
2021-04-05 17:05:30 -0700

    now

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,62 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider, Textarea, Code, VStack, Button, } from "@chakra-ui/react"
 
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-    console.log(service);
+
   };
   useEffect(() => {
     startService();
   }, []);
 
-  const onClick = () => {
-    console.log(input);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    })
+    console.log(result)
+    setCode(result.outputFiles[0].text)
   };
 
   return (
-    <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-    </div>
+
+    <ChakraProvider>
+
+      <VStack align="center" justify="center" my={30} spacing={30}>
+        <Textarea
+          bg="blue.50"
+          color="blue.900"
+          w="50%"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div>
+          <Button colorScheme="whatsapp" onClick={onClick}>Submit</Button>
+        </div>
+        <Code>{code}</Code>
+      </VStack>
+
+    </ChakraProvider>
+
   );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+
